d8c37fd59b0c7d0ce7a8813f1be5b9517735c05e
Alexander Matheisen <AlexanderMatheisen@ish.de>
2012-03-31 22:23:42 +0300

    Modified import script (milestones) - still not finished

--- import/import.sh
+++ import/import.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# OpenLinkMap Copyright (C) 2010 Alexander Matheisen
+# OpenRailwayMap Copyright (C) 2010 Alexander Matheisen
 # This program comes with ABSOLUTELY NO WARRANTY.
 # This is free software, and you are welcome to redistribute it under certain conditions.
-# See http://wiki.openstreetmap.org/wiki/OpenLinkMap for details.
+# See http://wiki.openstreetmap.org/wiki/OpenRailwayMap for details.
 
 
 # working directory, please change
@@ -32,36 +32,30 @@ echo ""
 # convert planet file, ~ 25 min
 echo "Converting planet file"
 echo ""
-osmconvert old.pbf --drop-relations --out-o5m >temp.o5m
+osmconvert old.pbf --out-o5m >temp.o5m
 echo ""
 
 
-# filter planet file, ~ 35 min
+# pre-filter planet file, ~ 35 min
 echo "Filtering planet file"
 echo ""
-osmfilter temp.o5m --keep="wikipedia= wikipedia:*= contact:phone= website= url= phone= fax= email= addr:email= image= url:official= contact:website= addr:phone= phone:mobile= contact:mobile= addr:fax= contact:email= contact:fax= image:panorama= opening_hours=" --out-o5m >temp-olm.o5m
-
-osmfilter temp.o5m --keep="amenity=bus_station highway=bus_stop railway=station railway=halt railway=tram_stop amenity=parking" --out-o5m >temp-nextobjects.o5m
+osmfilter temp.o5m --keep="railway=" --out-o5m >temp-railways.o5m
 rm temp.o5m
 echo ""
 
 
-# create centroids, remove not-node elements, ~ 2 min
-echo "Creating centroids, removing elements"
-echo ""
-osmconvert temp-olm.o5m --all-to-nodes --max-objects=50000000 --out-o5m >temp.o5m
-rm temp-olm.o5m
-osmfilter temp.o5m --drop-relations --drop-ways --keep-nodes="wikipedia= wikipedia:*= contact:phone= website= url= phone= fax= email= addr:email= image= url:official= contact:website= addr:phone= phone:mobile= contact:mobile= addr:fax= contact:email= contact:fax= image:panorama= opening_hours=" --fake-lonlat --fake-author --out-osm >temp.osm
-rm temp.o5m
-osmosis-0.40.1/bin/osmosis --rx file="temp.osm" enableDateParsing="no" --s --wx file="old-olm.osm"
-rm temp.osm
+# TODO AB HIER ANPASSEN
+# filter different object types, ~ ?? min
+# osmfilter temp-railways.o5m --drop-relations --drop-ways --keep="railway=milestone" --out-o5m >temp-milestones.o5m
 
-osmconvert temp-nextobjects.o5m --all-to-nodes --max-objects=50000000 --out-o5m >temp.o5m
-rm temp-nextobjects.o5m
-osmfilter temp.o5m --drop-relations --drop-ways --keep-nodes="amenity=bus_station highway=bus_stop railway=station railway=halt railway=tram_stop amenity=parking" --fake-lonlat --fake-author --out-osm >temp.osm
-rm temp.o5m
-osmosis-0.40.1/bin/osmosis --rx file="temp.osm" enableDateParsing="no" --s --wx file="old-nextobjects.osm"
+
+# some pre-processing of the filtered data, ?? min
+echo "Processing milestones..."
+osmfilter temp-milestones.o5m --fake-lonlat --fake-author --out-osm >temp.osm
+rm temp-milestones.o5m
+osmosis-0.40.1/bin/osmosis --rx file="temp.osm" enableDateParsing="no" --s --wx file="old-milestones.osm"
 rm temp.osm
+rm temp-railways.o5m
 echo ""
 
 
@@ -69,8 +63,6 @@ echo ""
 echo "Importing in database"
 echo ""
 php import.php
-osmconvert old-olm.osm --drop-author --out-o5m >old-olm.o5m
-rm old-olm.osm
-osmconvert old-nextobjects.osm --drop-author --out-o5m >old-nextobjects.o5m
-rm old-nextobjects.osm
+osmconvert old-milestones.osm --drop-author --out-o5m >old-milestones.o5m
+rm old-milestones.osm
 echo ""
