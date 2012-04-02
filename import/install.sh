5f4abc73ef4529813178bfa1ea49b82f9495708f
Alexander Matheisen <AlexanderMatheisen@ish.de>
2012-04-02 20:29:32 +0200

    Finished database backend

--- import/install.sh
+++ import/install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# OpenLinkMap Copyright (C) 2010 Alexander Matheisen
+# OpenRailwayMap Copyright (C) 2010 Alexander Matheisen
 # This program comes with ABSOLUTELY NO WARRANTY.
 # This is free software, and you are welcome to redistribute it under certain conditions.
-# See http://wiki.openstreetmap.org/wiki/OpenLinkMap for details.
+# See http://wiki.openstreetmap.org/wiki/OpenRailwayMap for details.
 
 
 # run this script not automatically! you have to change paths and modify it to your own environment!
 
 # install necessary software
-yum install gzip zlib zlib-devel postgresql-server postgresql-libs postgresql postgresql-common postgis geoip GeoIP geoip-devel GeoIP-devel php-pecl-geoip
+yum install gzip zlib zlib-devel postgresql-server postgresql-libs postgresql postgresql-common postgis geoip GeoIP geoip-devel GeoIP-devel php-pecl-geoip libxml2 libxml2-devel bzip2 bzip2-devel proj proj-devel protobuf protobuf-devel protobuf-python protobuf-compiler python-psycopg2 protobuf-c protobuf-c-devel postgresql-devel
 
 pecl install geoip
 
@@ -26,132 +26,31 @@ wget -O - http://m.m.i24.cc/osmupdate.c | cc -x c - -o osmupdate
 wget -O - http://m.m.i24.cc/osmfilter.c |cc -x c - -o osmfilter
 wget -O - http://m.m.i24.cc/osmconvert.c | cc -x c - -lz -o osmconvert
 
+cd /import/bin
+svn co http://svn.openstreetmap.org/applications/utils/export/osm2pgsql/
+cd osm2pgsql
+./autogen.sh
+./configure
+sed -i 's/-g -O2/-O2 -march=native -fomit-frame-pointer/' Makefile
+make
 
-# set up database
+# set up the database
 su postgres
 createuser olm
-
-
-createdb -E UTF8 -O olm olm
-createlang plpgsql olm
-psql -d olm -f /usr/share/pgsql/contrib/postgis-64.sql
-psql -d olm -f /usr/share/pgsql/contrib/postgis-1.5/spatial_ref_sys.sql
-psql -d olm -f /usr/share/pgsql/contrib/hstore.sql
-psql -d olm -f /usr/share/pgsql/contrib/_int.sql
-
-echo "ALTER TABLE geometry_columns OWNER TO olm; ALTER TABLE spatial_ref_sys OWNER TO olm;"  | psql -d olm
-echo "ALTER TABLE geography_columns OWNER TO olm;"  | psql -d olm
-
-
-createdb -E UTF8 -O olm nextobjects
-createlang plpgsql nextobjects
-
-psql -d nextobjects -f /usr/share/pgsql/contrib/postgis-64.sql
-psql -d nextobjects -f /usr/share/pgsql/contrib/postgis-1.5/spatial_ref_sys.sql
-psql -d nextobjects -f /usr/share/pgsql/contrib/hstore.sql
-psql -d nextobjects -f /usr/share/pgsql/contrib/_int.sql
-
-echo "ALTER TABLE geometry_columns OWNER TO olm; ALTER TABLE spatial_ref_sys OWNER TO olm;"  | psql -d nextobjects
-echo "ALTER TABLE geography_columns OWNER TO olm;"  | psql -d nextobjects
-
-
-# database olm
-echo "CREATE TABLE nodes (id bigint, tags hstore);" | psql -d olm
-echo "SELECT AddGeometryColumn('nodes', 'geom', 4326, 'POINT', 2);" | psql -d olm
-echo "CREATE INDEX geom_index_nodes ON nodes USING GIST(geom);" | psql -d olm
-echo "CLUSTER nodes USING geom_index_nodes;" | psql -d olm
-echo "CREATE INDEX id_index_nodes ON nodes (id);" | psql -d olm
-echo "CLUSTER nodes USING id_index_nodes;" | psql -d olm
-echo "CREATE INDEX tag_index_nodes ON nodes USING GIST (tags);" | psql -d olm
-echo "CLUSTER nodes USING tag_index_nodes;" | psql -d olm
-
-echo "CREATE TABLE ways (id bigint, tags hstore);" | psql -d olm
-echo "SELECT AddGeometryColumn('ways', 'geom', 4326, 'POINT', 2);" | psql -d olm
-echo "CREATE INDEX geom_index_ways ON ways USING GIST(geom);" | psql -d olm
-echo "CLUSTER ways USING geom_index_ways;" | psql -d olm
-echo "CREATE INDEX id_index_ways ON ways (id);" | psql -d olm
-echo "CLUSTER ways USING id_index_ways;" | psql -d olm
-echo "CREATE INDEX tag_index_ways ON ways USING GIST (tags);" | psql -d olm
-echo "CLUSTER ways USING tag_index_ways;" | psql -d olm
-
-echo "CREATE TABLE relations (id bigint, tags hstore);" | psql -d olm
-echo "SELECT AddGeometryColumn('relations', 'geom', 4326, 'POINT', 2);" | psql -d olm
-echo "CREATE INDEX geom_index_relations ON relations USING GIST(geom);" | psql -d olm
-echo "CLUSTER relations USING geom_index_relations;" | psql -d olm
-echo "CREATE INDEX id_index_relations ON relations (id);" | psql -d olm
-echo "CLUSTER relations USING id_index_relations;" | psql -d olm
-echo "CREATE INDEX tag_index_relations ON relations USING GIST (tags);" | psql -d olm
-echo "CLUSTER relations USING tag_index_relations;" | psql -d olm
-
-echo "GRANT all ON nodes TO olm;" | psql -d olm
-echo "GRANT all ON ways TO olm;" | psql -d olm
-echo "GRANT all ON relations TO olm;" | psql -d olm
-
-echo "GRANT truncate ON nodes TO olm;" | psql -d olm
-echo "GRANT truncate ON ways TO olm;" | psql -d olm
-echo "GRANT truncate ON relations TO olm;" | psql -d olm
-
-echo "ALTER TABLE nodes OWNER TO olm;" | psql -d olm
-echo "ALTER TABLE ways OWNER TO olm;" | psql -d olm
-echo "ALTER TABLE relations OWNER TO olm;" | psql -d olm
-
-
-# database nextobjects
-echo "CREATE TABLE nodes (id bigint, tags hstore);" | psql -d nextobjects
-echo "SELECT AddGeometryColumn('nodes', 'geom', 4326, 'POINT', 2);" | psql -d nextobjects
-echo "CREATE INDEX geom_index_nodes ON nodes USING GIST(geom);" | psql -d nextobjects
-echo "CLUSTER nodes USING geom_index_nodes;" | psql -d nextobjects
-echo "CREATE INDEX id_index_nodes ON nodes (id);" | psql -d nextobjects
-echo "CLUSTER nodes USING id_index_nodes;" | psql -d nextobjects
-echo "CREATE INDEX tag_index_nodes ON nodes USING GIST (tags);" | psql -d nextobjects
-echo "CLUSTER nodes USING tag_index_nodes;" | psql -d nextobjects
-
-echo "CREATE TABLE ways (id bigint, tags hstore);" | psql -d nextobjects
-echo "SELECT AddGeometryColumn('ways', 'geom', 4326, 'POINT', 2);" | psql -d nextobjects
-echo "CREATE INDEX geom_index_ways ON ways USING GIST(geom);" | psql -d nextobjects
-echo "CLUSTER ways USING geom_index_ways;" | psql -d nextobjects
-echo "CREATE INDEX id_index_ways ON ways (id);" | psql -d nextobjects
-echo "CLUSTER ways USING id_index_ways;" | psql -d nextobjects
-echo "CREATE INDEX tag_index_ways ON ways USING GIST (tags);" | psql -d nextobjects
-echo "CLUSTER ways USING tag_index_ways;" | psql -d nextobjects
-
-echo "CREATE TABLE relations (id bigint, tags hstore);" | psql -d nextobjects
-echo "SELECT AddGeometryColumn('relations', 'geom', 4326, 'POINT', 2);" | psql -d nextobjects
-echo "CREATE INDEX geom_index_relations ON relations USING GIST(geom);" | psql -d nextobjects
-echo "CLUSTER relations USING geom_index_relations;" | psql -d nextobjects
-echo "CREATE INDEX id_index_relations ON relations (id);" | psql -d nextobjects
-echo "CLUSTER relations USING id_index_relations;" | psql -d nextobjects
-echo "CREATE INDEX tag_index_relations ON relations USING GIST (tags);" | psql -d nextobjects
-echo "CLUSTER relations USING tag_index_relations;" | psql -d nextobjects
-
-echo "GRANT all ON nodes TO olm;" | psql -d nextobjects
-echo "GRANT all ON ways TO olm;" | psql -d nextobjects
-echo "GRANT all ON relations TO olm;" | psql -d nextobjects
-
-echo "GRANT truncate ON nodes TO olm;" | psql -d nextobjects
-echo "GRANT truncate ON ways TO olm;" | psql -d nextobjects
-echo "GRANT truncate ON relations TO olm;" | psql -d nextobjects
-
-echo "ALTER TABLE nodes OWNER TO olm;" | psql -d nextobjects
-echo "ALTER TABLE ways OWNER TO olm;" | psql -d nextobjects
-echo "ALTER TABLE relations OWNER TO olm;" | psql -d nextobjects
-
+createdb -E UTF8 -O olm railmap
+createlang plpgsql railmap
+psql -d railmap -f /usr/share/pgsql/contrib/postgis-64.sql
+psql -d railmap -f /usr/share/pgsql/contrib/postgis-1.5/spatial_ref_sys.sql
+psql -d railmap -f /usr/share/pgsql/contrib/hstore.sql
 
 # access
-echo "CREATE ROLE apache;" | psql -d olm
-
-echo "GRANT SELECT ON nodes TO apache;" | psql -d nextobjects
-echo "GRANT SELECT ON ways TO apache;" | psql -d nextobjects
-echo "GRANT SELECT ON relations TO apache;" | psql -d nextobjects
-echo "GRANT SELECT ON nodes TO apache;" | psql -d olm
-echo "GRANT SELECT ON ways TO apache;" | psql -d olm
-echo "GRANT SELECT ON relations TO apache;" | psql -d olm
-
-echo "CREATE ROLE w3_user1;" | psql -d olm
-
-echo "GRANT SELECT ON nodes TO w3_user1;" | psql -d nextobjects
-echo "GRANT SELECT ON ways TO w3_user1;" | psql -d nextobjects
-echo "GRANT SELECT ON relations TO w3_user1;" | psql -d nextobjects
-echo "GRANT SELECT ON nodes TO w3_user1;" | psql -d olm
-echo "GRANT SELECT ON ways TO w3_user1;" | psql -d olm
-echo "GRANT SELECT ON relations TO w3_user1;" | psql -d olm
+echo "ALTER TABLE geometry_columns OWNER TO olm; ALTER TABLE spatial_ref_sys OWNER TO olm;"  | psql -d railmap
+echo "ALTER TABLE geography_columns OWNER TO olm;"  | psql -d railmap
+echo "CREATE ROLE apache;" | psql -d railmap
+echo "GRANT SELECT ON railmap_point TO apache;" | psql -d railmap
+echo "GRANT SELECT ON railmap_line TO apache;" | psql -d railmap
+echo "GRANT SELECT ON railmap_polygon TO apache;" | psql -d railmap
+echo "CREATE ROLE w3_user1;" | psql -d railmap
+echo "GRANT SELECT ON railmap_point TO w3_user1;" | psql -d railmap
+echo "GRANT SELECT ON railmap_line TO w3_user1;" | psql -d railmap
+echo "GRANT SELECT ON railmap_polygon TO w3_user1;" | psql -d railmap
--- import/update.sh
+++ import/update.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-# OpenLinkMap Copyright (C) 2010 Alexander Matheisen
+# OpenRailwayMap Copyright (C) 2010 Alexander Matheisen
 # This program comes with ABSOLUTELY NO WARRANTY.
 # This is free software, and you are welcome to redistribute it under certain conditions.
-# See http://wiki.openstreetmap.org/wiki/OpenLinkMap for details.
+# See http://wiki.openstreetmap.org/wiki/OpenRailwayMap for details.
 
+# some commands are marked as comments because on the server they are merged with OpenLinkMap
 
 # working directory, please change
 cd /home/www/sites/194.245.35.149/site/import
 PATH="$PATH:/home/www/sites/194.245.35.149/site/import/bin"
 
 
-
 # update planet file, ~ 50 min
-echo "Updating planet file"
-echo ""
-date -u +%s > timestamp_tmp
-osmupdate old.pbf new.pbf --max-merge=2 --hourly --drop-author -v
-rm old.pbf
-mv new.pbf old.pbf
+#echo "Updating planet file"
+#echo ""
+#date -u +%s > timestamp_tmp
+#osmupdate old.pbf new.pbf --max-merge=2 --hourly --drop-author -v
+#rm old.pbf
+#mv new.pbf old.pbf
 echo ""
 
 
@@ -29,58 +29,29 @@ osmconvert old.pbf --out-o5m >temp.o5m
 echo ""
 
 
-# filter planet file, ~ 35 min
+# pre-filter planet file, ~ 20 min
 echo "Filtering planet file"
 echo ""
-osmfilter temp.o5m --keep="wikipedia= wikipedia:*= contact:phone= website= url= phone= fax= email= addr:email= image= url:official= contact:website= addr:phone= phone:mobile= contact:mobile= addr:fax= contact:email= contact:fax= image:panorama= opening_hours=" --out-o5m >temp-olm.o5m
-
-osmfilter temp.o5m --keep="amenity=bus_station highway=bus_stop railway=station railway=halt railway=tram_stop amenity=parking" --out-o5m >temp-nextobjects.o5m
+osmfilter temp.o5m --keep="railway=" --out-o5m >new-railways.o5m
 rm temp.o5m
 echo ""
 
 
-# create centroids, remove not-node elements, ~ 2 min
-echo "Creating centroids, removing elements"
-echo ""
-osmconvert temp-olm.o5m --all-to-nodes --max-objects=50000000 --out-o5m >temp.o5m
-rm temp-olm.o5m
-osmfilter temp.o5m --drop-relations --drop-ways --keep-nodes="wikipedia= wikipedia:*= contact:phone= website= url= phone= fax= email= addr:email= image= url:official= contact:website= addr:phone= phone:mobile= contact:mobile= addr:fax= contact:email= contact:fax= image:panorama= opening_hours=" --fake-lonlat --fake-author --out-osm >temp.osm
-rm temp.o5m
-osmosis-0.40.1/bin/osmosis --rx file="temp.osm" enableDateParsing="no" --s --wx file="olm.osm"
-rm temp.osm
-osmconvert olm.osm --drop-author --out-o5m >olm.o5m
-rm olm.osm
-
-osmconvert temp-nextobjects.o5m --all-to-nodes --max-objects=50000000 --out-o5m >temp.o5m
-rm temp-nextobjects.o5m
-osmfilter temp.o5m --drop-relations --drop-ways --keep-nodes="amenity=bus_station highway=bus_stop railway=station railway=halt railway=tram_stop amenity=parking" --fake-lonlat --fake-author --out-osm >temp.osm
-rm temp.o5m
-osmosis-0.40.1/bin/osmosis --rx file="temp.osm" enableDateParsing="no" --s --wx file="nextobjects.osm"
-rm temp.osm
-osmconvert nextobjects.osm --drop-author --out-o5m >nextobjects.o5m
-rm nextobjects.osm
-echo ""
-
-
-# generate diffs, ~ 2 min
+# generate diffs, ~ ?? min
 echo "Generate diffs"
 echo ""
-osmconvert old-olm.o5m olm.o5m --diff-contents >olm.o5c
-rm old-olm.o5m
-mv olm.o5m old-olm.o5m
-
-osmconvert old-nextobjects.o5m nextobjects.o5m --diff-contents >nextobjects.o5c
-rm old-nextobjects.o5m
-mv nextobjects.o5m old-nextobjects.o5m
+osmconvert old-railways.o5m new-railways.o5m --diff-contents >changes.osc
+rm old-railways.o5m
+mv new-railways.o5m old-railways.o5m
 echo ""
 
 
-# updating of database by diffs, ~ 2 min
-echo "Updating of database"
+# load data into database, ~ ?? min
+echo "Updating database"
 echo ""
-php update.php
-rm olm.o5c
-rm nextobjects.o5c
+osm2pgsql --database railmap --append --prefix railmap --slim --style railmap.style --hstore --cache 512 changes.osc
+rm changes.osc
 rm timestamp
 mv timestamp_tmp timestamp
 echo ""
+echo "Finished."
