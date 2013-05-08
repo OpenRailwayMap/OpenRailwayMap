dd0cd472957f17781190d65d0639f84ec14618af
Alexander Matheisen <AlexanderMatheisen@ish.de>
2013-05-08 23:38:38 +0200

    Removed osmosis - no longer needed

--- import/bin/osmosis-0.42/script/contrib/dump_apidb.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/usr/bin/pg_dump -O -v -f "apidb_0.6.sql" api06
--- import/bin/osmosis-0.42/script/pgsnapshot_schema_0.6_upgrade_4-5.sql
+++
@@ -1,5 +0,0 @@
--- Add the relation members primary key.
-ALTER TABLE ONLY relation_members ADD CONSTRAINT pk_relation_members PRIMARY KEY (relation_id, sequence_id);
-
--- Upgrade the schema version.
-UPDATE schema_info SET version = 5;
