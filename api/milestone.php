cd9c0b626e708a1573adeb89a3335f866d393bc1
Alexander Matheisen <AlexanderMatheisen@ish.de>
2013-05-12 13:30:59 +0200

    Added API for getting lat/lon of a certain milestone on a certain line, better database debugging, some minor edits

---
+++ api/milestone.php
@@ -0,0 +1,26 @@
+<?php
+	/*
+	OpenRailwayMap Copyright (C) 2012 Alexander Matheisen
+	This program comes with ABSOLUTELY NO WARRANTY.
+	This is free software, and you are welcome to redistribute it under certain conditions.
+	See http://wiki.openstreetmap.org/wiki/OpenRailwayMap for details.
+	*/
+
+
+	require_once("functions.php");
+
+	if (!isValidLine($_GET['line']))
+		die("Param z invalid or missing.");
+	if (!isValidPosition($_GET['position']))
+		die("Param x invalid or missing.");
+
+	$result = getMilestonePosition($_GET['line'], $_GET['position']);
+	if (!$result)
+		echo "NULL";
+	else
+	{
+		header("Content-Type: text/html; charset=UTF-8");
+		echo "<meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\">";
+		echo $result['0']." ".$result['1'];
+	}
+?>
