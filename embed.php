7846d84e29e8dded1c2ed5dcfaef73b4e8f54993
Alexander Matheisen <AlexanderMatheisen@ish.de>
2013-06-24 22:56:51 +0200

    Repaired embed mode, some code cleanup

--- embed.php
+++ embed.php
@@ -8,12 +8,12 @@
 
 	require_once("api/functions.php");
 
-	if (isset($_GET['lang']) && in_array($_GET['lang'], $langs))
+	if (isset($_GET['lang']) && array_key_exists($_GET['lang'], $langs))
 		$lang = $_GET['lang'];
 	else
 		$lang = getUserLang();
 
-	require_once("locales/".$lang.".php");
+	includeLocale($lang);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<? echo $lang; ?>" lang="<? echo $lang; ?>">
@@ -21,7 +21,7 @@
 		<title><?=$appname?></title>
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 		<meta http-equiv="content-language" content="<? echo $lang; ?>" />
-		<meta name="keywords" content="openstreetmap, openlinkmap, alexander matheisen, rurseekatze, openlayers, osm, matheisen, olm" />
+		<meta name="keywords" content="openstreetmap, openrailwaymap, alexander matheisen, rurseekatze, openlayers, osm, matheisen, orm, eisenbahnkarte, bahnkarte, railmap, railway, railways, eisenbahn, streckenkarte" />
 		<meta name="title" content="<?=$appname?>" />
 		<meta name="author" content="rurseekatze, Alexander Matheisen" />
 		<meta name="publisher" content="rurseekatze, Alexander Matheisen" />
@@ -35,7 +35,18 @@
 		<meta http-equiv="content-script-type" content="text/javascript" />
 		<meta http-equiv="content-style-type" content="text/css" />
 		<link rel="stylesheet" type="text/css" href="css/embed.css" />
-		<script type="text/javascript" src="js/OpenLayers.js"></script>
+		<link rel="stylesheet" href="css/leaflet.css" />
+		<link rel="stylesheet" href="css/L.Control.Zoomslider.css" />
+		<!--[if lte IE 8]>
+			<link rel="stylesheet" href="css/leaflet.ie.css" />
+			<link rel="stylesheet" href="css/L.Control.Zoomslider.ie.css" />
+		<![endif]-->
+		<script type="text/javascript" src="js/leaflet-0.5.1.js"></script>
+		<script type="text/javascript" src="js/kothic.js"></script>
+		<script type="text/javascript" src="js/kothic-leaflet.js"></script>
+		<script type="text/javascript" src="js/L.Control.Zoomslider.js"></script>
+		<script type="text/javascript" src="js/L.TileLayer.Grayscale.js"></script>
+		<script type="text/javascript" src="styles/style.js"></script>
 		<?php
 			// params
 			echo "<script type=\"text/javascript\">\n";
@@ -45,6 +56,7 @@
 				if (!isset($type))
 					$type = isset($_GET['objecttype']) ? $_GET['objecttype'] : null;
 				echo "type : ".(isset($type) ? ("\"".$type."\"") : ("null")).",\n";
+				echo "ext : ".((isset($_GET['ext']) && ($_GET['ext'] == 1)) ? ("true") : ("false")).",\n";
 				echo "lat : ";
 					if (isset($_GET['lat']))
 						echo $_GET['lat'].",\n";
@@ -68,20 +80,20 @@
 							echo "null,\n";
 					}
 				echo "zoom : ".(isset($_GET['zoom']) ? ($_GET['zoom']) : ("null")).",\n";
+				echo "bounded : ".(((isset($_GET['bounded'])) && ($_GET['bounded'] == 1)) ? 1 : 0).",\n";
 				echo "offset : ".(isset($_GET['offset']) ? ($_GET['offset']) : ("null")).",\n";
-				echo "lang : \"".$lang."\"\n";
+				echo "searchquery : \"".(isset($_GET['q']) ? ($_GET['q']) : (""))."\",\n";
+				echo "lang : \"".$_GET['lang']."\"\n";
 				echo "};\n";
 			echo "</script>\n";
 		?>
-		<script type="text/javascript" src="locales/<? echo $lang; ?>.js"></script>
-		<script type="text/javascript" src="api/langfile.php?lang=<? echo $lang; ?>"></script>
-		<script type="text/javascript" src="js/OpenStreetMap.js"></script>
-		<script type="text/javascript" src="js/embed.js"></script>
+		<script type="text/javascript" src="api/jstranslations.php?lang=<? echo $lang; ?>"></script>
+		<script type="text/javascript" src="js/functions.js"></script>
 	</head>
-	<body onload="createMap();">
+	<body onload="createMap(true);">
 		<div id="mapFrame" class="mapFrame">
 			<noscript>
-				<p><b><?=$translations['captions']['nojavascriptheader']?></b><br /><?=$translations['captions']['nojavascripttext']?></p>
+				<p><b><?=_("Javascript is not activated")?></b><br /><?=_("Javascript is needed to show the map and run this website. Please turn on Javascript in your browser settings.")?></p>
 			</noscript>
 		</div>
 	</body>
