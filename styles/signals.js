5e160df735351b260c2fbdd1ca7e52ba6338f1e4
Alexander Matheisen <AlexanderMatheisen@ish.de>
2013-09-19 18:16:56 +0200

    Added german Ks signals to signals layer & some cleanup

--- styles/signals.js
+++ styles/signals.js
@@ -14,7 +14,7 @@
             s_default['watch-modified'] = 'true';
         }
 
-        if (((type == 'node' && tags['railway'] == 'signal' && tags['railway:signal:main'] == 'hp' && tags['railway:signal:main:form'] == 'semaphore' && tags['railway:signal:main:function'] == 'entry') && zoom >= 14) || ((type == 'node' && tags['railway'] == 'signal' && tags['railway:signal:main'] == 'hp' && tags['railway:signal:main:form'] == 'semaphore' && tags['railway:signal:main:function'] == 'block') && zoom >= 14)) {
+        if (((type == 'node' && tags['railway'] == 'signal' && tags['railway:signal:main'] == 'hp' && tags['railway:signal:main:form'] == 'semaphore') && zoom >= 14)) {
             s_default['z-index'] = 10000;
             s_default['text'] = MapCSS.e_localize(tags, 'ref');
             s_default['text-offset'] = 11;
@@ -28,35 +28,7 @@
             s_default['icon-height'] = '16';
         }
 
-        if (((type == 'node' && tags['railway'] == 'signal' && tags['railway:signal:main'] == 'hp' && tags['railway:signal:main:form'] == 'light' && tags['railway:signal:main:function'] == 'entry') && zoom >= 14) || ((type == 'node' && tags['railway'] == 'signal' && tags['railway:signal:main'] == 'hp' && tags['railway:signal:main:form'] == 'light' && tags['railway:signal:main:function'] == 'block') && zoom >= 14)) {
-            s_default['z-index'] = 10000;
-            s_default['text'] = MapCSS.e_localize(tags, 'ref');
-            s_default['text-offset'] = 11;
-            s_default['text-size'] = '12';
-            s_default['text-color'] = 'black';
-            s_default['text-halo-radius'] = 1;
-            s_default['text-halo-color'] = 'white';
-            s_default['font-weight'] = 'bold';
-            s_default['icon-image'] = 'icons/de:hp-light.png';
-            s_default['icon-width'] = '16';
-            s_default['icon-height'] = '16';
-        }
-
-        if (((type == 'node' && tags['railway'] == 'signal' && tags['railway:signal:main'] == 'hp' && tags['railway:signal:main:form'] == 'semaphore' && tags['railway:signal:main:function'] == 'exit') && zoom >= 17)) {
-            s_default['z-index'] = 10000;
-            s_default['text'] = MapCSS.e_localize(tags, 'ref');
-            s_default['text-offset'] = 11;
-            s_default['text-size'] = '12';
-            s_default['text-color'] = 'black';
-            s_default['text-halo-radius'] = 1;
-            s_default['text-halo-color'] = 'white';
-            s_default['font-weight'] = 'bold';
-            s_default['icon-image'] = 'icons/de:hp-semaphore.png';
-            s_default['icon-width'] = '16';
-            s_default['icon-height'] = '16';
-        }
-
-        if (((type == 'node' && tags['railway'] == 'signal' && tags['railway:signal:main'] == 'hp' && tags['railway:signal:main:form'] == 'light' && tags['railway:signal:main:function'] == 'exit') && zoom >= 17)) {
+        if (((type == 'node' && tags['railway'] == 'signal' && tags['railway:signal:main'] == 'hp' && tags['railway:signal:main:form'] == 'light') && zoom >= 14)) {
             s_default['z-index'] = 10000;
             s_default['text'] = MapCSS.e_localize(tags, 'ref');
             s_default['text-offset'] = 11;
@@ -126,6 +98,48 @@
             s_default['icon-height'] = '16';
         }
 
+        if (((type == 'node' && tags['railway'] == 'signal' && tags['railway:signal:distant'] == 'ks' && tags['railway:signal:distant:form'] == 'light' && tags['railway:signal:distant:states'] == 'ks1;ks2') && zoom >= 14)) {
+            s_default['z-index'] = 10000;
+            s_default['text'] = MapCSS.e_localize(tags, 'ref');
+            s_default['text-offset'] = 11;
+            s_default['text-size'] = '12';
+            s_default['text-color'] = 'black';
+            s_default['text-halo-radius'] = 1;
+            s_default['text-halo-color'] = 'white';
+            s_default['font-weight'] = 'bold';
+            s_default['icon-image'] = 'icons/de:ks-distant.png';
+            s_default['icon-width'] = '16';
+            s_default['icon-height'] = '16';
+        }
+
+        if (((type == 'node' && tags['railway'] == 'signal' && tags['railway:signal:main'] == 'ks' && tags['railway:signal:main:form'] == 'light' && tags['railway:signal:main:states'] == 'hp0;ks1') && zoom >= 14)) {
+            s_default['z-index'] = 10000;
+            s_default['text'] = MapCSS.e_localize(tags, 'ref');
+            s_default['text-offset'] = 11;
+            s_default['text-size'] = '12';
+            s_default['text-color'] = 'black';
+            s_default['text-halo-radius'] = 1;
+            s_default['text-halo-color'] = 'white';
+            s_default['font-weight'] = 'bold';
+            s_default['icon-image'] = 'icons/de:ks-main.png';
+            s_default['icon-width'] = '16';
+            s_default['icon-height'] = '16';
+        }
+
+        if (((type == 'node' && tags['railway'] == 'signal' && tags['railway:signal:combined'] == 'ks' && tags['railway:signal:combined:form'] == 'light' && tags['railway:signal:combined:states'] == 'hp0;ks1;ks2') && zoom >= 14)) {
+            s_default['z-index'] = 10000;
+            s_default['text'] = MapCSS.e_localize(tags, 'ref');
+            s_default['text-offset'] = 11;
+            s_default['text-size'] = '12';
+            s_default['text-color'] = 'black';
+            s_default['text-halo-radius'] = 1;
+            s_default['text-halo-color'] = 'white';
+            s_default['font-weight'] = 'bold';
+            s_default['icon-image'] = 'icons/de:ks-combined.png';
+            s_default['icon-width'] = '16';
+            s_default['icon-height'] = '16';
+        }
+
         if (Object.keys(s_default).length) {
             style['default'] = s_default; }
         return style;
@@ -142,27 +156,42 @@
             height: 16, 
             offset: 16
         },
-        'icons/de:sh-light-dwarf.png': {
+        'icons/de:ks-combined.png': {
             width: 16, 
             height: 16, 
             offset: 32
         },
-        'icons/de:sh-light.png': {
+        'icons/de:ks-distant.png': {
             width: 16, 
             height: 16, 
             offset: 48
         },
-        'icons/de:sh-semaphore-dwarf.png': {
+        'icons/de:ks-main.png': {
             width: 16, 
             height: 16, 
             offset: 64
         },
-        'icons/de:sh-semaphore.png': {
+        'icons/de:sh-light-dwarf.png': {
             width: 16, 
             height: 16, 
             offset: 80
+        },
+        'icons/de:sh-light.png': {
+            width: 16, 
+            height: 16, 
+            offset: 96
+        },
+        'icons/de:sh-semaphore-dwarf.png': {
+            width: 16, 
+            height: 16, 
+            offset: 112
+        },
+        'icons/de:sh-semaphore.png': {
+            width: 16, 
+            height: 16, 
+            offset: 128
         }
-    }, external_images = [], presence_tags = [], value_tags = ['ref', 'railway:signal:minor:form', 'railway:signal:main:function', 'railway', 'railway:signal:minor:height', 'railway:signal:main', 'railway:signal:minor', 'railway:signal:main:form'];
+    }, external_images = [], presence_tags = [], value_tags = ['ref', 'railway:signal:combined:states', 'railway:signal:main', 'railway', 'railway:signal:minor:height', 'railway:signal:minor:form', 'railway:signal:minor', 'railway:signal:main:form', 'railway:signal:main:states', 'railway:signal:distant', 'railway:signal:combined', 'railway:signal:distant:states', 'railway:signal:combined:form', 'railway:signal:distant:form'];
 
     MapCSS.loadStyle('signals', restyle, sprite_images, external_images, presence_tags, value_tags);
     MapCSS.preloadExternalImages('signals');
