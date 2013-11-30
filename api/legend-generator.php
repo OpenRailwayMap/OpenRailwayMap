ea4d4bc976d1a9ba2d400112db5d3d06a7ba9847
Alexander Matheisen <AlexanderMatheisen@ish.de>
2013-11-30 19:39:43 +0100

    Changed file extenstion

--- api/legend-generator.php
+++ api/legend-generator.php
@@ -16,7 +16,7 @@
 	includeLocale($lang);
 
 	$zoom = isset($_GET['zoom']) ? ($_GET['zoom']) : (null);
-	$filename = isset($_GET['style']) ? ("../styles/".$_GET['style'].".ml") : (null);
+	$filename = isset($_GET['style']) ? ("../styles/".$_GET['style'].".json") : (null);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<? echo $lang; ?>" lang="<? echo $lang; ?>">
