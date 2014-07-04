102e2810d61abf25279eb93e7a5d3d67fc72c7c4
Alexander Matheisen <AlexanderMatheisen@ish.de>
2014-07-05 00:29:07 +0200

    Clicking on label changes style selection

--- js/canvas-map.js
+++ js/canvas-map.js
@@ -170,7 +170,7 @@ function getStyleSelection()
 {
 	gEBI('styleSelectionBar').innerHTML = '<form id="styleSelection"><b>'+translations['styleSelection']+':</b><br /><p>';
 	for (var i=0; i<MapCSS.availableStyles.length; i++)
-		gEBI('styleSelectionBar').innerHTML += '<input onchange="setStyle(this.value)" type="radio" name="style" value="'+MapCSS.availableStyles[i]+'">'+translations['style.'+MapCSS.availableStyles[i]]+'<br />';
+		gEBI('styleSelectionBar').innerHTML += '<label><input onchange="setStyle(this.value)" type="radio" name="style" value="'+MapCSS.availableStyles[i]+'">'+translations['style.'+MapCSS.availableStyles[i]]+'</label><br />';
 	gEBI('styleSelectionBar').innerHTML += '</p></form>';
 }
 
