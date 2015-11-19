b6e87012fe9640129d583cf3251d3d92c3c30aad
Rolf Eike Beer <eike@sf-mail.de>
2015-11-19 20:33:33 +0100

    use automatic loading of Flattr button
    This avoids the delay until the map itself is loaded I can sometimes observe.

--- canvas.php
+++ canvas.php
@@ -157,6 +157,7 @@
 			(function(){
 				var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
 				s.type = 'text/javascript';
+				s.mode = 'auto';
 				s.async = true;
 				s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
 				t.parentNode.insertBefore(s, t);
