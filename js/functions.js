ae063758156bfe11583d7834bc27de66a96f17cb
Rolf Eike Beer <eike@sf-mail.de>
2016-03-28 18:51:14 +0200

    mobile: assume mobile platform if "Jolla" is in the Browser string

--- js/functions.js
+++ js/functions.js
@@ -154,6 +154,7 @@ function isMobileDevice()
 		|| navigator.userAgent.match(/iPod/i)
 		|| navigator.userAgent.match(/BlackBerry/i)
 		|| navigator.userAgent.match(/Windows Phone/i)
+		|| navigator.userAgent.match(/Jolla/)
 	)
 		return true;
 
