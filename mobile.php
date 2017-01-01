83a8e5ed5ad228536019629e310383175b528c44
Alexander Matheisen <AlexanderMatheisen@ish.de>
2017-01-01 02:42:33 +0100

    Removed Piwik tracking code Piwik is no longer used for analyzing website usage

--- mobile.php
+++ mobile.php
@@ -84,21 +84,6 @@
 		<script type="text/javascript" src="js/mobilemenu.js"></script>
 		<script type="text/javascript" src="js/functions.js"></script>
 		<script type="text/javascript" src="js/mobile.js"></script>
-		<!-- Piwik -->
-		<script type="text/javascript">
-			var _paq = _paq || [];
-			_paq.push(["trackPageView"]);
-			_paq.push(["enableLinkTracking"]);
-
-			(function() {
-			var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.openrailwaymap.org/";
-			_paq.push(["setTrackerUrl", u+"piwik.php"]);
-			_paq.push(["setSiteId", "2"]);
-			var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
-			g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
-			})();
-		</script>
-		<!-- End Piwik Code -->
 	</head>
 	<body onload="createMap(false);">
 		<div id="header">
