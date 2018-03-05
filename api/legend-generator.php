2e9973f077bd4c16d0af9f7e7c69eb5f583ab468
Alexander Matheisen <AlexanderMatheisen@ish.de>
2018-03-05 13:36:42 +0100

    Update doctype and html root tag

--- api/legend-generator.php
+++ api/legend-generator.php
@@ -19,7 +19,7 @@
 	$filename = isset($_GET['style']) ? ("../styles/".$_GET['style'].".json") : (null);
 ?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $lang; ?>" lang="<?php echo $lang; ?>">
+<html>
 	<head>
 		<title><?=$appname?></title>
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
