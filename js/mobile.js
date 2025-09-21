f3d4e730738b7f4a51d64f0e5daeec988396ff01
Michael Reichert <nakaner@gmx.net>
2025-09-21 11:46:11 +0200

    Drop subdomains [abc].tiles.openrailwaymap.org
    The tile server has supported HTTP/2 since 25 April 2024. Therefore,
    this hack is not necessary any more.

--- js/mobile.js
+++ js/mobile.js
@@ -9,7 +9,7 @@ See https://wiki.openstreetmap.org/wiki/OpenRailwayMap for details.
 window.openrailwaymap = {
 	'root': params['urlbase'],
 	'apiUrl': 'https://api.openrailwaymap.org/v2/',
-	'tiledir': 'https://{s}.tiles.openrailwaymap.org/',
+	'tiledir': 'https://tiles.openrailwaymap.org/',
 	'availableStyles': {
 		"standard": "Infrastructure",
 		"maxspeed": "Maxspeeds",
