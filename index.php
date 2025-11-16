cfe29153ee470423bebb5dac6f597f15b48a2578
Michael Reichert <nakaner@gmx.net>
2025-11-16 13:04:45 +0100

    Force browsers to reload the page every 24 hours
    If the API changes, users might contact the old API because they haven't
    refreshed the web application for multiple days.

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 		<meta http-equiv="content-language" content="<?php echo $lang; ?>" />
+		<meta http-equiv="refresh" content="86400">
 
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
